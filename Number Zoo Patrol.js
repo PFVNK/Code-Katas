10a33a7a02b61da5f5b76a16898db29b1a3f6484
Patrick Heflin <33630354+PFVNK@users.noreply.github.com>
2019-01-27 15:56:28 -0600

    2019

--- Number Zoo Patrol.js
+++ Number Zoo Patrol.js
@@ -15,12 +15,3 @@ function findNumber(array) {
 
     return array.length + 1
 }
-
-
-console.log(findNumber([1, 3, 4, 5, 6, 7, 8]), 2);
-console.log(findNumber([7, 8, 1, 2, 4, 5, 6]), 3);
-console.log(findNumber([1, 2, 3, 5]), 4);
-console.log(findNumber([1, 3]), 2);
-console.log(findNumber([2, 3, 4]), 1);
-console.log(findNumber([13, 11, 10, 3, 2, 1, 4, 5, 6, 9, 7, 8]), 12);
-console.log(findNumber([1, 2, 3]), 4);
