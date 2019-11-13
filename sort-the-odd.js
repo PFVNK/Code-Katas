b61612ca8adf0590d70891a30f5d680d8271ba4c
Patrick Heflin <patrickslx@yahoo.com>
2019-11-13 08:53:31 -0600

    2019

--- sort-the-odd.js
+++ sort-the-odd.js
@@ -33,6 +33,12 @@ function sortArray(array) {
 }
 
 
+function sortArray(array) {
+  const odd = array.filter((number) => number % 2).sort((a, b) => a - b);
+  return array.map((number) => number % 2 ? odd.shift() : number);
+}
+
+
 console.log(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4])
 // console.log(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0])
 // console.log(sortArray([]), [])
