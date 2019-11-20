a650c8d6b180168c0a3d5481ec1f84be01e194aa
Patrick Heflin <patrickslx@yahoo.com>
2019-11-19 22:26:18 -0600

    2019

---
+++ remove-the-minimum.js
@@ -0,0 +1,26 @@
+//remove-the-minimum   https://www.codewars.com/kata/remove-the-minimum/train/javascript
+
+//description: Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+
+
+function removeSmallest(numbers) {
+  if (numbers.length > 0) {
+    let smallest = [...numbers].sort().shift()
+
+    let smallIndex = numbers.findIndex(x => x === smallest)
+
+    return [...numbers].filter((number, index) => index !== smallIndex)
+
+  } else {
+
+    return []
+  }
+}
+
+
+
+console.log(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+console.log(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4]);
+console.log(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1]);
+console.log(removeSmallest([]), []);
