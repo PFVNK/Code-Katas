8fc2996b64f5bb954365e9fb57ce773af3a20477
Patrick Heflin <patrickslx@yahoo.com>
2020-06-25 11:13:32 -0500

    2020

---
+++ Kebabize.js
@@ -0,0 +1,26 @@
+//Kebabize https://www.codewars.com/kata/57f8ff867a28db569e000c4a/train/javascript
+
+//Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+
+
+function kebabize(str) {
+  str = str.split('').filter(val => isNaN(val))
+
+  let kabab = []
+
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      kabab.push(`-${str[i].toLowerCase()}`)
+    } else {
+      kabab.push(str[i])
+    }
+  }
+
+  return kabab.join('')
+}
+
+
+
+console.log(kebabize('myCamelCasedString'), 'my-camel-cased-string');
+console.log(kebabize('myCamelHas3Humps'), 'my-camel-has-humps');
