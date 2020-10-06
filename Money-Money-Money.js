fc942b03f32802c82fca379d43b13c190dfe1080
Patrick Heflin <patrickslx@yahoo.com>
2020-10-06 09:18:20 -0500

    2020

---
+++ Money-Money-Money.js
@@ -0,0 +1,21 @@
+//Money, Money, Money
+
+
+
+function calculateYears(principal, interest, tax, desired) {
+  let money = principal
+  let years = 0
+
+  while (desired > money) {
+    years++
+    money = money + (money * interest - (money * interest * tax))
+  }
+
+  return years
+}
+
+
+
+console.log(calculateYears(1000, 0.05, 0.18, 1100), 3)
+console.log(calculateYears(1000, 0.01625, 0.18, 1200), 14)
+console.log(calculateYears(1000, 0.05, 0.18, 1000), 0)
