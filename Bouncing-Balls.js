816840eb31126015c568ef019beee5fe7b1adf83
Patrick Heflin <patrickslx@yahoo.com>
2020-12-31 11:52:45 -0600

    2020

--- Bouncing-Balls.js
+++ Bouncing-Balls.js
@@ -3,16 +3,20 @@
 
 
 function bouncingBall(h, bounce, window) {
-  let ballSighting = 0
-  while (h > window) {
-    h = h * bounce
-    ballSighting += 2
-  }
+  if (h > 0 && bounce > 0 && bounce < 1 && window < h) {
+    let ballSighting = 0
+    while (h > window) {
+      h = h * bounce
+      ballSighting += 2
+    }
 
-  return ballSighting - 1
+    return ballSighting - 1
+  }
 }
 
 
 
 console.log(bouncingBall(3.0, 0.66, 1.5), 3);
 console.log(bouncingBall(30.0, 0.66, 1.5), 15);
+console.log(bouncingBall(2, 0.5, 1), 1);
+console.log(bouncingBall(30, 0.4, 10), 3);
