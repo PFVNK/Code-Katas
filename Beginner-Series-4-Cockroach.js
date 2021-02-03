55eaddb19154fc2d334e10138d9e667af32cdfa1
Patrick Heflin <patrickslx@yahoo.com>
2021-02-03 10:31:24 -0600

    2021

---
+++ Beginner-Series-4-Cockroach.js
@@ -0,0 +1,13 @@
+//Beginner Series #4 Cockroach
+
+
+
+function cockroachSpeed(s) {
+  return Math.floor(s * 2.7778)
+}
+
+
+
+console.log(cockroachSpeed(1.08), 30);
+console.log(cockroachSpeed(1.09), 30);
+console.log(cockroachSpeed(0), 0);
