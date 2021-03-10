feb110542dee9151067c85d5b1fe8c48b139f585
Patrick Heflin <patrickslx@yahoo.com>
2021-03-10 12:36:58 -0600

    2021

---
+++ Make-them-bark.js
@@ -0,0 +1,22 @@
+//Make them bark!
+
+
+
+function Dog(name, breed, sex, age) {
+  this.name = name;
+  this.breed = breed;
+  this.sex = sex;
+  this.age = age;
+}
+
+Dog.prototype.bark = function () {
+  return 'Woof!'
+}
+
+
+
+var apollo = new Dog('Apollo', 'Dobermann', 'male', '4');
+var zeus = new Dog('Zeus', 'Dobermann', 'male', '4');
+
+console.log(apollo.bark(), 'Woof!');
+console.log(zeus.bark(), 'Woof!');
