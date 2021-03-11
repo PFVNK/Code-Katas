566ce39344cb3b3b27c60b3b77754049b1b51bac
Patrick Heflin <patrickslx@yahoo.com>
2021-03-11 10:16:15 -0600

    2021

---
+++ Maximum-Multiple.js
@@ -0,0 +1,20 @@
+//Maximum Multiple
+
+
+
+function maxMultiple(divisor, bound) {
+  while (bound % divisor !== 0 && bound <= bound && bound > 0) {
+    bound--
+  }
+
+  return bound
+}
+
+
+
+console.log(maxMultiple(2, 7), 6);
+console.log(maxMultiple(3, 10), 9);
+console.log(maxMultiple(7, 17), 14);
+console.log(maxMultiple(10, 50), 50);
+console.log(maxMultiple(37, 200), 185);
+console.log(maxMultiple(7, 100), 98);
