a0c93c87dcaf77b7c56f9cf7bd42e4d8ef0adf6e
Patrick Heflin <patrickslx@yahoo.com>
2021-03-12 09:54:38 -0600

    2021

---
+++ Remove-duplicates-from-list.js
@@ -0,0 +1,13 @@
+//Remove duplicates from list
+
+
+
+function distinct(a) {
+  return Array.from(new Set(a))
+}
+
+
+
+console.log(distinct([1]), [1]);
+console.log(distinct([1, 2]), [1, 2]);
+console.log(distinct([1, 1, 2]), [1, 2]);
