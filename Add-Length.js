38114b303581d75964d13baad9c0075e02c4f772
Patrick Heflin <patrickslx@yahoo.com>
2021-04-12 14:13:33 -0500

    2021

---
+++ Add-Length.js
@@ -0,0 +1,16 @@
+//Add Length
+
+
+
+function addLength(str) {
+  return str.split(' ').map(word => word + ' ' + word.length)
+}
+
+function addLength(str) {
+  return str.split(' ').map(word => `${word} ${word.length}`)
+}
+
+
+
+console.log(addLength('apple ban'), ["apple 5", "ban 3"]);
+console.log(addLength('you will win'), ["you 3", "will 4", "win 3"]);
