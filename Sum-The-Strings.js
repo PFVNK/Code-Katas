9d743c9f6acd1b5c6fff9b1fa52a1accb44c8a80
Patrick Heflin <patrickslx@yahoo.com>
2021-05-25 15:23:40 -0500

    2021

---
+++ Sum-The-Strings.js
@@ -0,0 +1,12 @@
+//Sum The Strings
+
+
+
+function sumStr(a, b) {
+  return (Math.abs(a) + Math.abs(b)).toString()
+}
+
+
+
+console.log(sumStr("4", "5"), "9");
+console.log(sumStr("34", "5"), "39");
