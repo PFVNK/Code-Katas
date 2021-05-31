eb94bd517f287bb1d6766babea293a3d355cc399
Patrick Heflin <patrickslx@yahoo.com>
2021-05-31 14:45:45 -0500

    2021

---
+++ Twice-as-old.js
@@ -0,0 +1,14 @@
+//Twice as old
+
+
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - sonYearsOld * 2)
+}
+
+
+
+console.log(twiceAsOld(36, 7), 22);
+console.log(twiceAsOld(55, 30), 5);
+console.log(twiceAsOld(42, 21), 0);
+console.log(twiceAsOld(22, 1), 20);
