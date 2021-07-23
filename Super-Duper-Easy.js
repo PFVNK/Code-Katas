bfa6a7cda62e380539717844582bf1de61bc90ad
Patrick Heflin <patrickslx@yahoo.com>
2021-07-23 08:36:05 -0500

    2021

---
+++ Super-Duper-Easy.js
@@ -0,0 +1,24 @@
+//Super Duper Easy
+
+
+
+// function problem(x) {
+//   if (typeof x === "string") return 'Error'
+
+//   return x * 50 + 6
+// }
+
+const problem = x => typeof x === "string" ? 'Error' : x * 50 + 6
+
+
+
+console.log(problem("hello"), "Error");
+console.log(problem(1), 56);
+console.log(problem(5), 256);
+console.log(problem(0), 6);
+console.log(problem(1.2), 66);
+console.log(problem(3), 156);
+console.log(problem("RyanIsCool"), "Error");
+console.log(problem(-3), -144);
+console.log(problem(""), "Error");
+console.log(problem(0.03), 7.5);
