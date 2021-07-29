d839cd4e9b7de7fe45497a8b3782ac2da35478b8
Patrick Heflin <patrickslx@yahoo.com>
2021-07-29 10:29:17 -0500

    2021

---
+++ shorter-concat-reverse-longer.js
@@ -0,0 +1,8 @@
+//shorter_reverse_longer
+
+
+
+function shorter_reverse_longer(a, b) {
+  return a.length >= b.length ? b + a.split('').reverse().join('') + b :
+    a + b.split('').reverse().join('') + a;
+}
