e48306242c2a11ba39e728432e8bd75e79a9f837
Patrick Heflin <patrickslx@yahoo.com>
2021-10-25 09:45:48 -0500

    2021

---
+++ Thinkful-String-Drills-Repeater.js
@@ -0,0 +1,14 @@
+//Thinkful - String Drills: Repeater
+
+
+
+// function repeater(string, n){
+//   return string.repeat(n)
+// }
+
+const repeater = (string, n) => string.repeat(n)
+
+
+console.log(repeater('a', 5), 'aaaaa')
+console.log(repeater('Na', 16), 'NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa')
+console.log(repeater('Wub ', 6), 'Wub Wub Wub Wub Wub Wub ')
