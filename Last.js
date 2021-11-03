ddb5f177d26195b7f68a28d3ba6ded5d26438e42
Patrick Heflin <patrickslx@yahoo.com>
2021-11-03 16:32:27 -0500

    2021

--- Last.js
+++ Last.js
@@ -3,11 +3,16 @@
 
 
 function last(list) {
-  console.log(list[1])
+  if (Array.isArray(list) || typeof list === 'string') {
+    return list[list.length - 1]
+  } else {
+    return arguments[arguments.length - 1]
+  }
 }
 
 
 
 console.log(last([1, 2, 3, 4, 5]), 5);   //-- array
 console.log(last("abcde"), "e");     //-- string
-console.log(last(1, "b", 3, "d", 5), 5);//-- arguments
+console.log(last(1, "b", 3, "d", 5), 5);
+console.log(last('a'), 'a');//-- arguments
