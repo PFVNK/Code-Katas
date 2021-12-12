c92d4245477f641e223513ec0a000def2219c46a
Patrick Heflin <patrickslx@yahoo.com>
2021-12-11 21:43:18 -0600

    2021

---
+++ Bin-to-Decimal.js
@@ -0,0 +1,14 @@
+//Bin to Decimal
+
+
+
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
+
+
+
+console.log(binToDec('1001001'), 73)
+console.log(binToDec('10010110101010010111001'), 4936889)
+
+
