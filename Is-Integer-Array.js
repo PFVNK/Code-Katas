d0066b28e8ce0a7c552766d2d31e5961fa52894e
Patrick Heflin <patrickslx@yahoo.com>
2021-12-21 09:31:38 -0600

    2021

--- Is-Integer-Array.js
+++ Is-Integer-Array.js
@@ -9,11 +9,25 @@
 
 
 function isIntArray(arr) {
-
+  if (arr.every((number) => !isNaN(number))) return true
+  return false
 }
 
 
+function isIntArray(a) {
+  if (!Array.isArray(a)) {
+    return false;
+  }
+  for (var i = 0, l = a.length; i < l; i++) {
+    var n = a[i];
+    if (n !== ~~n) {
+      return false;
+    }
+  }
+  return true;
+}
 
-console.log(isIntArray([]), true, "Input: []");
-console.log(isIntArray([1, 2, 3, 4]), true, "Input: [1, 2, 3, 4]");
-console.log(isIntArray([1, 2, 3, NaN]), false, "Input: [1, 2, 3, NaN]");
+console.log(isIntArray([]), true);
+console.log(isIntArray([1, 2, 3, 4]), true);
+console.log(isIntArray([1, 2, 3, NaN]), false);
+console.log(isIntArray(['-1']), false);
